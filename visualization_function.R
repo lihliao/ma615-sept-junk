1185facb43ffb18f9d1930a9bc8851df976f986b
Jacob <jacobchen@dhcp-wifi-8021x-155-41-66-43.bu.edu>
2021-11-01 16:00:34 -0400

    add a smooth line function

--- visualization_function.R
+++ visualization_function.R
@@ -7,9 +7,13 @@ ggfun <- function(dat, x.var, y.var){
   
   p <- ggplot(data = dat,
                 aes(x = x.var,y = y.var)) +
-    geom_point()
+    geom_point()+
+    geom_smooth(formula = y~x)
   
   return(p)
 }
 
+
+# you did a really nice job, I learn a lot from you. But I think you can add a smooth line in your plot and make it colorful
+#which can make the graph much more attractive
 
